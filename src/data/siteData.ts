56459cd4a61c54d548abf4d442c5a86b4cdb694a
Aaron <158839001+jobintobias@users.noreply.github.com>
2024-11-27 13:27:47 -0600

    first_commit

---
+++ src/data/siteData.ts
@@ -0,0 +1,17 @@
+export const baseData = {
+  title: "Aaron in Tech",
+  description: "Aaron's tech journey",
+};
+
+export const socialData = [
+  {
+    title: "github",
+    href: "https://github.com/",
+    icon: "github",
+  },
+  {
+    title: "bluesky",
+    href: "https://bsky.app",
+    icon: "bluesky",
+  },
+];
